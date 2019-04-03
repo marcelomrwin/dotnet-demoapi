a68130af741125ac7a48944131b7ba87196f96e1
Marcelo Sales <marcelo.d.sales@gmail.com>
2019-04-03 00:52:35 -0300

    dotnet 2.1

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace demoapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.Configure<AppConfig>(Configuration);
             services.AddScoped<AuthenticationFilterAttribute>();
             services.AddSingleton<IPersonRepository, PersonRepository>();
